716240116cacf5151c3928c11a91313446a215cd
Nic Sevic <nasevic@gmail.com>
2019-03-19 16:20:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (let i = 0; i<array.length-1; i++){
-    if (array[i] === findFn){
+    if (array[i] === true){
       return array[i]
     }
     else {
@@ -28,15 +28,4 @@ function findBy(array, findFn) {
     }
   }
 }
-describe('findBy(array, findFn)', function() {
-  it('finds the a value that satisfies `findFn`', function() {
-    const c = Math.floor(Math.random() * 100)
 
-    expect(findBy([1, 1, 1, 1, c, 1, 2], n => n === c)).toEqual(c)
-  })
-
-  it('returns `null` if no match found', function() {
-    expect(findBy(["dog", "cat", "cow"], a => a === "moose")).toEqual(null)
-  })
-})
-})
