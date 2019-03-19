16fde8ac168fb12b6b274f46cd1bb99d43e7bb84
Nic Sevic <nasevic@gmail.com>
2019-03-19 16:20:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,11 +19,24 @@ function keepGoing(array, changeValue, skipValue) {
 }
 
 function findBy(array, findFn) {
-  for ( var i in array){
+  for (let i = 0; i<array.length-1; i++){
     if (array[i] === findFn){
       return array[i]
     }
-    else { return null
+    else { 
+      return null
     }
   }
 }
+describe('findBy(array, findFn)', function() {
+  it('finds the a value that satisfies `findFn`', function() {
+    const c = Math.floor(Math.random() * 100)
+
+    expect(findBy([1, 1, 1, 1, c, 1, 2], n => n === c)).toEqual(c)
+  })
+
+  it('returns `null` if no match found', function() {
+    expect(findBy(["dog", "cat", "cow"], a => a === "moose")).toEqual(null)
+  })
+})
+})
