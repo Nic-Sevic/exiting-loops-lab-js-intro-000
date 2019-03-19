dbd332fb370c7f28be5080a346ec1f7c10a11985
Nic Sevic <nasevic@gmail.com>
2019-03-19 16:45:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (let i = 0; i<array.length-1; i++){
-    if (array[i] === true){
+    if (findFn === true){
       return array[i]
     }
     else {
