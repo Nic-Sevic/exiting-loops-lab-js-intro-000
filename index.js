262919f483f86f3cf4c66728ff8fda155d56d48d
Nic Sevic <nasevic@gmail.com>
2019-03-19 16:49:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (let i = 0; i<array.length-1; i++){
-    if (array[i] === findFn()){
+    if (true === findFn(array[i])){
       return array[i]
     }
     else {
